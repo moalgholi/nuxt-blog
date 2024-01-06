61a49bda795838eaaca096829ca74eab54c57d8c
Mohammad Ali Gholizadeh <moal.gholi@gmail.com>
2024-01-06 12:42:41 +0330

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,37 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  app: {
+    head: {
+      title: "نمونه وبسایت بلاگ",
+      htmlAttrs: {
+        dir: "rtl",
+        lang: "fa",
+      },
+      meta: [
+        { charset: "utf-8" },
+        {
+          name: "viewport",
+          content: "width=device-width, initial-scale=1, user-scalable=no",
+        },
+        { hid: "description", name: "description", content: "" },
+        { name: "format-detection", content: "telephone=no" },
+        { name: "theme-color", content: "#0d244c" },
+      ],
+      script: [
+        // <script src="https://myawesome-lib.js"></script>
+        // { src: "https://awesome-lib.js" },
+      ],
+      link: [
+        // <link rel="stylesheet" href="https://myawesome-lib.css">
+        { rel: "icon", type: "image/webp", href: "/favicon.webp" },
+        // { rel: "manifest", href: "/manifest.json" },
+      ],
+      // please note that this is an area that is likely to change
+      style: [
+        // <style type="text/css">:root { color: red }</style>
+        // { children: ":root { color: red }", type: "text/css" },
+      ],
+    },
+  },
+});
